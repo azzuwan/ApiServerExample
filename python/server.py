9d6128458b56a305406d5a52e061b17bcf397eca
Azzuwan Aziz <azzuwan@gmail.com>
2017-06-26 19:28:10 +0800

    Simple version of Python Web API service with MongoEngine

---
+++ python/server.py
@@ -0,0 +1,16 @@
+from japronto import Application
+from services.articles import ArticleService
+
+article_service = ArticleService()
+
+def index(req):
+	return  req.Response(text='Yuhuuuu')
+
+def articles(req):
+	docs = article_service.all()	
+	return req.Response(text=docs.to_json())
+
+app = Application()
+app.router.add_route('/', index)
+app.router.add_route('/articles', articles)
+app.run(debug=True)
